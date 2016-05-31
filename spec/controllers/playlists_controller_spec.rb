e68b3b7ebf1b10774c5c4169b3a2dea48e3e779e
Lucy Fox <lucyreneefox@gmail.com>
2016-05-31 10:27:33 -0600

    user able to sign in via heroku, small styling changes and refactoring

--- spec/controllers/playlists_controller_spec.rb
+++ spec/controllers/playlists_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe PlaylistsController, type: :controller do
     context "with valid params" do
       it "creates a new playlist" do
         expect {
-          post :create, {:playlist => attributes_for(:playlist)}
+          post :create, { :playlist => attributes_for(:playlist) }
         }.to change(Playlist, :count).by(1)
       end
 
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  it { is_expected.to validate_presence_of(:title)}
-  it { is_expected.to validate_uniqueness_of(:title)}
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_uniqueness_of(:title) }
 end
